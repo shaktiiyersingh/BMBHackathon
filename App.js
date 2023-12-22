813014fc3d4e8cc6beae505f18958b3cf1deddae
Sujit Swain <sujitswain2023@gmail.com>
2023-12-22 15:59:11 +0530

    optimize app.js

--- App.js
+++ App.js
@@ -1,211 +1,62 @@
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from '@react-navigation/stack';
-import React, {useState, useEffect} from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { CartProvider } from "./src/context/CartContext";
 
-import Header  from "./src/components/Header/Header";
-import Registration from './src/components/Registration/Registration';
-import Login from './src/components/Login/Login';
-import Dashboard from './src/components/DashBoard/Dashboard';
-import ProductDisplay from './src/components/ProductDisplay/ProductDisplay';
-import CartDisplay from './src/components/CartDisplay/CartDisplay';
+import Header from "./src/components/Header/Header";
+import Login from "./src/components/Login/Login";
+import Registration from "./src/components/Registration/Registration";
+import Dashboard from "./src/components/DashBoard/Dashboard";
+import ProductDisplay from "./src/components/ProductDisplay/ProductDisplay";
+import CartDisplay from "./src/components/CartDisplay/CartDisplay";
 import Address from "./src/components/Address/Address";
 import AddAddress from "./src/components/Address/AddAddress";
 import Inventory from "./src/components/Inventry/Inventry";
-import Payment from './src/components/Payment/Payment';
+import Payment from "./src/components/Payment/Payment";
 import OrderStatus from "./src/components/Payment/OrderStatus";
-
-import {CartProvider} from './src/context/CartContext'
 
 const Stack = createStackNavigator();
 
-export default () => {
-    return(
-        <CartProvider>
-            <NavigationContainer>
-                <Stack.Navigator initialRouteName="Login">
-                    <Stack.Screen 
-                        name = "Login"
-                        component = {Login}
-                        options = {
-                            {
-                                headerTitle: () => <Header name = "Login" showCartIcon={false} />,
-                                headerStyle: {
-                                    height: 90,
-                                    borderBottomLeftRadius: 20,
-                                    borderBottomRightRadius: 20,
-                                    backgroundColor: '#00e4d0',
-                                    shadowColor: '#000',
-                                    elevation: 25
-                                }
-                            }
-                        }
-                    />
+const screens = [
+  { name: "Login", component: Login, title: "Login", showCartIcon: false },
+  { name: "Registration", component: Registration, title: "Registration", showCartIcon: false },
+  { name: "Dashboard", component: Dashboard, title: "Dashboard", showCartIcon: true },
+  { name: "ProductDisplay", component: ProductDisplay, title: "Products", showCartIcon: true },
+  { name: "CartDisplay", component: CartDisplay, title: "Cart", showCartIcon: true },
+  { name: "Address", component: Address, title: "Address", showCartIcon: true },
+  { name: "AddAddress", component: AddAddress, title: "AddAddress", showCartIcon: true },
+  { name: "Inventory", component: Inventory, title: "Inventory", showCartIcon: true },
+  { name: "Payment", component: Payment, title: "Payment", showCartIcon: false },
+  { name: "OrderStatus", component: OrderStatus, title: "OrderStatus", showCartIcon: false },
+];
 
-                    <Stack.Screen 
-                        name = "Registration"
-                        component = {Registration}
-                        options = {
-                            {
-                                headerTitle: () => <Header name = "Registration" showCartIcon={false} />,
-                                headerStyle: {
-                                    height: 90,
-                                    borderBottomLeftRadius: 20,
-                                    borderBottomRightRadius: 20,
-                                    backgroundColor: '#00e4d0',
-                                    shadowColor: '#000',
-                                    elevation: 25
-                                }
-                            }
-                        }
-                    />
+const App = () => (
+  <CartProvider>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        {screens.map(({ name, component, title, showCartIcon }) => (
+          <Stack.Screen
+            key={name}
+            name={name}
+            component={component}
+            options={{
+              headerTitle: () => <Header name={title} showCartIcon={showCartIcon} />,
+              headerStyle: {
+                height: 90,
+                borderBottomLeftRadius: 20,
+                borderBottomRightRadius: 20,
+                backgroundColor: "#00e4d0",
+                shadowColor: "#000",
+                elevation: 25,
+              },
+            }}
+          />
+        ))}
+      </Stack.Navigator>
+    </NavigationContainer>
+  </CartProvider>
+);
 
-                    <Stack.Screen 
-                        name = "Dashboard"
-                        component = {Dashboard}
-                        options = {
-                            {
-                                headerLeft: () => null,
-                                headerTitle: () => <Header name = "Dashboard" showCartIcon={true}/>,
-                                headerStyle: {
-                                    height: 90,
-                                    borderBottomLeftRadius: 20,
-                                    borderBottomRightRadius: 20,
-                                    backgroundColor: '#00e4d0',
-                                    shadowColor: '#000',
-                                    elevation: 25
-                                }
-                            }
-                        }
-                    />
-
-                    <Stack.Screen 
-                        name = "ProductDisplay"
-                        component = {ProductDisplay}
-                        options = {
-                            {
-                                headerTitle: () => <Header name = "Products" showCartIcon={true} />,
-                                headerStyle: {
-                                    height: 90,
-                                    borderBottomLeftRadius: 20,
-                                    borderBottomRightRadius: 20,
-                                    backgroundColor: '#00e4d0',
-                                    shadowColor: '#000',
-                                    elevation: 25
-                                }
-                            }
-                        }
-                    />
-                    
-                    <Stack.Screen 
-                        name = "CartDisplay"
-                        component = {CartDisplay}
-                        options = {
-                            {
-                                headerTitle: () => <Header name = "Cart" showCartIcon={true} />,
-                                headerStyle: {
-                                    height: 90,
-                                    borderBottomLeftRadius: 20,
-                                    borderBottomRightRadius: 20,
-                                    backgroundColor: '#00e4d0',
-                                    shadowColor: '#000',
-                                    elevation: 25
-                                }
-                            }
-                        }
-                    />
-
-                    <Stack.Screen 
-                        name = "Address"
-                        component = {Address}
-                        options = {
-                            {
-                                headerTitle: () => <Header name = "Address" showCartIcon={true} />,
-                                headerStyle: {
-                                    height: 90,
-                                    borderBottomLeftRadius: 20,
-                                    borderBottomRightRadius: 20,
-                                    backgroundColor: '#00e4d0',
-                                    shadowColor: '#000',
-                                    elevation: 25
-                                }
-                            }
-                        }
-                    />
-
-                    <Stack.Screen 
-                        name = "AddAddress"
-                        component = {AddAddress}
-                        options = {
-                            {
-                                headerTitle: () => <Header name = "AddAddress" showCartIcon={true} />,
-                                headerStyle: {
-                                    height: 90,
-                                    borderBottomLeftRadius: 20,
-                                    borderBottomRightRadius: 20,
-                                    backgroundColor: '#00e4d0',
-                                    shadowColor: '#000',
-                                    elevation: 25
-                                }
-                            }
-                        }
-                    />
-                    
-                    <Stack.Screen 
-                        name = "Inventory"
-                        component = {Inventory}
-                        options = {
-                            {
-                                headerTitle: () => <Header name = "Inventory" />,
-                                headerStyle: {
-                                    height: 90,
-                                    borderBottomLeftRadius: 20,
-                                    borderBottomRightRadius: 20,
-                                    backgroundColor: '#00e4d0',
-                                    shadowColor: '#000',
-                                    elevation: 25
-                                }
-                            }
-                        }
-                    />
-
-                    <Stack.Screen 
-                        name = "Payment"
-                        component = {Payment}
-                        options = {
-                            {
-                                headerTitle: () => <Header name = "Payment" />,
-                                headerStyle: {
-                                    height: 90,
-                                    borderBottomLeftRadius: 20,
-                                    borderBottomRightRadius: 20,
-                                    backgroundColor: '#00e4d0',
-                                    shadowColor: '#000',
-                                    elevation: 25
-                                }
-                            }
-                        }
-                    />
-                    
-                    <Stack.Screen 
-                        name = "OrderStatus"
-                        component = {OrderStatus}
-                        options = {
-                            {
-                                headerLeft: () => null,
-                                headerTitle: () => <Header name = "OrderStatus" />,
-                                headerStyle: {
-                                    height: 90,
-                                    borderBottomLeftRadius: 20,
-                                    borderBottomRightRadius: 20,
-                                    backgroundColor: '#00e4d0',
-                                    shadowColor: '#000',
-                                    elevation: 25
-                                }
-                            }
-                        }
-                    />
-                </Stack.Navigator>
-            </NavigationContainer>
-        </CartProvider>
-    )
-}
+export default () => (
+  <App />
+);
